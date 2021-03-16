456aef4ce4b59098020164325a791e29daaed06a
Blaine Anderson <banderson@silverchair.com>
2021-03-16 12:04:44 -0400

    Add QuoteBox component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <HelloWorld msg="Get a Random Star Wars Quote" />
+        <QuoteBox />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
+    import QuoteBox from './components/QuoteBox.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+      name: 'App',
+      components: {
+          HelloWorld,
+          QuoteBox
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
 </style>
---
+++ src/components/QuoteBox.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="quote-box">
+        <div id="text">This will be the quote.</div>
+        <div id="author">This will be the author.</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'QuoteBox'
+    }
+</script>
+
+<style>
+    #quote-box {
+        margin: 0 auto;
+        border: 1px solid #000;
+    }
+</style>
